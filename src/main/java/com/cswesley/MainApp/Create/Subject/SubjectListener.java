3667c8deefad6f1c882ce84ffc541f96f463a8b7
CSWesley <booked1234512@gmail.com>
2022-01-18 13:57:07 -0800

    Subject creation is complete.

--- src/main/java/com/cswesley/MainApp/Create/Subject/SubjectListener.java
+++ src/main/java/com/cswesley/MainApp/Create/Subject/SubjectListener.java
@@ -1,32 +1,54 @@
 package com.cswesley.MainApp.Create.Subject;
 
+import com.cswesley.Utils.Utilities;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class SubjectListener {
 
-    public ActionListener createSubjectListener(JButton button) {
-
-        return e -> {
-            if (e.getSource() == button) {
-                // here is where the subject is created.
-            }
-        };
-    }
+    JButton saveButton;
+    JTextField enterSubject;
+    private JFrame newSubjectFrame;
 
     private void insertSubjectToDatabase() {
+        Firestore db = FirestoreClient.getFirestore();
 
+        DocumentReference docRef = db.collection("users").document(Utilities.username).collection("subjects").document(enterSubject.getText());
+        Map<String, Object> subject = new HashMap<>();
+        subject.put("name", enterSubject.getText());
+        subject.put("# of assignments", 0);
+        subject.put("# of assignments completed", 0);
+
+        docRef.set(subject);
     }
 
-    private boolean doesExist() {
+    private boolean doesExist() throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+
+        ApiFuture<QuerySnapshot> query = db.collection("users").document(Utilities.username).collection("subjects").get();
+        QuerySnapshot querySnapshot = query.get();
+
+        for (QueryDocumentSnapshot document : querySnapshot.getDocuments()) {
+            return document.getString("name").equals(enterSubject.getText());
+        }
+
         return false;
     }
 
     private void createSubjectFrame(ActionListener actionListener) {
-        JFrame newSubjectFrame = new JFrame("Create New Subject");
+        newSubjectFrame = new JFrame("Create New Subject");
         newSubjectFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         Container c = newSubjectFrame.getContentPane();
         c.setLayout(null);
@@ -38,12 +60,12 @@ public class SubjectListener {
         nameOfSubject.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
         c.add(nameOfSubject);
 
-        JTextField enterSubject = new JTextField();
+        enterSubject = new JTextField();
         enterSubject.setSize(140, 35);
         enterSubject.setLocation(55, 100);
         c.add(enterSubject);
 
-        JButton saveButton = new JButton("Save");
+        saveButton = new JButton("Save");
         saveButton.setSize(100, 40);
         saveButton.setLocation(75, 155);
         saveButton.addActionListener(actionListener);
@@ -53,5 +75,34 @@ public class SubjectListener {
         newSubjectFrame.setSize(265, 250);
         newSubjectFrame.setLocationRelativeTo(null);
         newSubjectFrame.setVisible(true);
+    }
+
+    public ActionListener createSubjectListener(JButton createSubject) {
+
+        return e -> {
+            if (e.getSource() == createSubject) {
+                // here is where the subject is created.
+                createSubjectFrame(al -> {
+                    if (al.getSource() == saveButton) {
+                        String subjectName = enterSubject.getText();
+                        if (subjectName.equals("")) {
+                            JOptionPane.showMessageDialog(null, "Please enter a subject name");
+                        } else {
+                            try {
+                                if (doesExist()) {
+                                    JOptionPane.showMessageDialog(null, "Subject already exists");
+                                } else {
+                                    insertSubjectToDatabase();
+                                    JOptionPane.showMessageDialog(null, "Subject created");
+                                    newSubjectFrame.dispose();
+                                }
+                            } catch (ExecutionException | InterruptedException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            }
+        };
     }
 }
